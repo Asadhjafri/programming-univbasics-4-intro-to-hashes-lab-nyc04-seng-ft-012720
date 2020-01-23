ebaafff9915fe14c6b23e6053954407e35132158
Asad Jafri <asadhjafri@gmail.com>
2020-01-23 22:10:25 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,21 +1,34 @@
 def new_hash
-  # return an empty hash
+  newhash = Hash.new
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  identification = Hash.new
+  identification = {"name" => "Asad", "age"=> 25}
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer = Hash.new
+  pioneer = {
+    :name => "Grace Hopper"
+  }
 end
 
 def id_generator
-  # return a hash with a key :id assigned to positive integer
+  id = Hash.new
+  id = {
+    :id => 5
+  }
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  person => Hash.new
+   person = {
+   name: => "Grace Hopper"
+   age: => 25
+ }
+
+#return a hash that includes the key and value parameters passed into this method
 end
 
 def read_from_hash(hash, key)
