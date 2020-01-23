e5c2d3e95e5ec32760b519da43ee1a8d8d72ebd0
Asad Jafri <asadhjafri@gmail.com>
2020-01-23 22:11:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,13 +22,7 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  person => Hash.new
-   person = {
-   name: => "Grace Hopper"
-   age: => 25
- }
-
-#return a hash that includes the key and value parameters passed into this method
+  {key => value}#return a hash that includes the key and value parameters passed into this method
 end
 
 def read_from_hash(hash, key)
